b5dff4e9885cd90be592ff520894336f3ec05b12
Arie Peretz <ariep20@gmail.com>
2020-10-20 21:58:56 +0300

    Initial Commit

---
+++ LoadingIndicator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoadingIndicator
+//
+//  Created by Arie Peretz on 20/10/2020.
+//  Copyright © 2020 Arie Peretz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
